71ed15f50661f8b17e3b8c9d8dac3bc3bb59889f
pc-PC\pc <836414757@qq.com>
2019-03-24 14:55:54 +0800

    so that's begin leaning kotlin

--- app/src/main/java/com/allen/MainActivity.kt
+++ app/src/main/java/com/allen/MainActivity.kt
@@ -2,12 +2,46 @@ package com.allen
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Printer
 
 class MainActivity : AppCompatActivity() {
+
+    val a: Int = 1
+    val b = 2
+    val c: Int = 3
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("sum of 1 and 2 is ${sum(1, 2)}")
+        printSum(100, 200)
     }
 
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun div(a: Int, b: Int): Int {
+        return a / b
+    }
+
+    fun multi(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun sub(a: Int, b: Int) = a - b
+
+    fun printSum(a: Int, b: Int) {
+        println("sum of $a and $b is ${sum(a, b)}")
+    }
+
+    fun difineVar() {
+        val a: Int = 4
+        val b = 2
+        val c: Int = 3
+        var x = 5
+
+    }
 }
---
+++ app/src/main/java/com/allen/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.allen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    private int sum(int a, int b){
+        return a+b;
+    }
+}
