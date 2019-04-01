77bb7d070867c363ab05337dea13b614651035e0
allen.wang <allen.wang@yff.com>
2019-04-01 09:49:55 +0800

    new Test Activity

--- app/src/main/java/com/allen/Animal.kt
+++ app/src/main/java/com/allen/Animal.kt
@@ -2,20 +2,34 @@ package com.allen
 
 class Animal {
 
-    fun Animal.print(){
+    fun Animal.print() {
         println("this is a expand method")
     }
 
-    fun MutableList<Int>.swap(index1:Int, index2:Int){
+    fun MutableList<Int>.swap(index1: Int, index2: Int) {
         val tmp = this[index1]
         this[index1] = this[index2]
         this[index2] = tmp
     }
 
-    fun main(args: Array<String>?){
+    fun main(args: Array<String>?) {
         val list = mutableListOf(1, 2, 3)
         list.swap(0, 2)
         println(list.toString())
     }
 
+    class C {
+        fun foo() {
+            println("成员函数")
+        }
+    }
+
+    fun C.foo() {
+        println("拓展函数")
+    }
+
+    fun test() {
+        val c = C()
+        c.foo()
+    }
 }
---
+++ app/src/main/java/com/allen/ThirdActivity.kt
@@ -0,0 +1,13 @@
+package com.allen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+        Animal().test()
+    }
+}
