29da0d51d42e8e68bcbaec7a97634256950a88f5
pc-PC\pc <836414757@qq.com>
2019-04-14 21:00:38 +0800

    函数变量

--- app/src/main/java/com/allen/HelloWorld.kt
+++ app/src/main/java/com/allen/HelloWorld.kt
@@ -2,4 +2,8 @@ package com.allen
 
 fun main(args: Array<String>) {
     println("Hello Kotlin")
+
+    var i = {x:Int, y:Long -> x+y }
+    println(i(1,2))
+    var j : (Int, Int)->Int = {x, y -> x+y}
 }
